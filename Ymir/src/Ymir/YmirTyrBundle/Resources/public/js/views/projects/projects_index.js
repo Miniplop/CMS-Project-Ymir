6c21c606a7a759f40afa363eafb36d656b4becb6
Jean <tiner@hotmail.fr>
2015-05-27 19:41:04 +0200

    backbone marche php app/console server:run! ajout de la structure d'App.js avec un factory

--- Ymir/src/Ymir/YmirTyrBundle/Resources/public/js/views/projects/projects_index.js
+++ Ymir/src/Ymir/YmirTyrBundle/Resources/public/js/views/projects/projects_index.js
@@ -2,39 +2,15 @@
 // view qui rend la liste des projets
   App.Views.ProjectsIndex = Backbone.View.extend({
         
-        el:".list-projet",
+    template: _.template($("#tpl-menu").html()),
       
-        tagName:"ul",
-
-        className :"projets",
-
-        attributes:"",
+    initialize: function() {
+        this.render();
+    },
       
-        events:{
-        },
-
-
-        initialize: function() {
-            _.bindAll(this,'render'); // Bind this to all view functions
-            this.collection = new App.Collections.Projects();
-            this.collection.on('add',this.render,this);
-            this.collection.fetch();
-        },
-
-        //this creates new rows in the table for each model in the collection
-        render: function() {
-             var $el = $(this.el), self = this; // Remarque du prof
-            // Console print
-            console.log(this.el);
-            console.log(this.$el);
-            //this.$el.empty(); // Erreur si vide
-            
-           this.collection.each(function(projet){
-               var projetView = new projet_view({model:projet});
-               self.$el.append(App.Views.ProjectIndex.el);
-           });
-            return this;
-        };
+    render: function(){
+        this.$el.html(this.template({}));
+    }
       
 });    
 
