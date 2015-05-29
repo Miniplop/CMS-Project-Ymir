b4cfbac0b31f5bc04e1dfaaef9a64f264172b9c1
Jean <tiner@hotmail.fr>
2015-05-29 19:21:33 +0200

    ajout des debut du routage

--- Ymir/src/Ymir/YmirTyrBundle/Resources/public/js/routers/ProjectListRouter.js
+++ Ymir/src/Ymir/YmirTyrBundle/Resources/public/js/routers/ProjectListRouter.js
@@ -2,13 +2,20 @@ var App = App || {};
 App.Routers.ProjectListRouter = Backbone.Router.extend({
     
     routes: {
-		'' : 'home',
-		'/download/:index': 'donwload'
+		'/' : 'home',
+        '/creative' : 'creative',
+        '/profil' : 'profil',
 	},
+    
 	home: function () {
 		alert('you are viewing home page');
 	},
-	donwload: function (index) {
-		alert('you tying to dl '+index);
+    
+    creative: function () {
+		alert('you are viewing creative page');
+	},
+    
+    profil: function () {
+		alert('you are viewing profil page');
 	}
 });
