e69e29b2e79c5231d5f0dfffd64937a026e3644f
Jean <tiner@hotmail.fr>
2015-06-01 18:37:52 +0200

    correc page en attentes d'un push fonctionelle de matheo

--- Ymir/src/Ymir/YmirTyrBundle/Resources/public/js/views/projects/ProjectPageView.js
+++ Ymir/src/Ymir/YmirTyrBundle/Resources/public/js/views/projects/ProjectPageView.js
@@ -25,13 +25,12 @@ App.Views.ProjectPageView = Backbone.View.extend({
     
     removePage: function (e){
          var id = $(e.currentTarget).data('id'); // Récupération de l'id <3 backbone
-         var page = this.model.pages.get(id);
+         var page = this.model.get("pages").get(id);
          page.destroy({},{
               success : function(){
-                 this.model.pages.remove(id); //
+                 this.model.get("pages").remove(id); //
              },
               error : function(){
-                 
              }
          });
     },
